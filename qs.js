e0e525024bb424a225079b852e0328aedac3a7e4
Colin <colin.diesh@gmail.com>
2017-10-17 08:57:11 -0400

    Fixup cocktail sort

--- qs.js
+++ qs.js
@@ -24,70 +24,16 @@ function shuffle(array) {
 }
 
 
-function bubblesort(array, target, width, draw, curr) {
-    var done = [];
-    var progress = [];
-    var arrlen = array.length / 4;
-    var sofar = 0;
-    for (let i = 0; i < arrlen; i++) {
-        progress[i] = i % width;
-    }
-    for (let i = 0; i < arrlen; i += width) {
-        done.push(false);
-        var ret = progress.slice(i, i + width);
-        var shuf = shuffle(ret);
-        for(let j = 0; j < width-sofar; j++) {
-            progress[i+j]=shuf[j];
-        }
-    }
-    sofar++;
-
-
-    var ret = setInterval(function () {
-        var flag = true;
-        for (let j = 0; j < arrlen / width; j++) {
-            flag &= done[j];
-        }
-        if (flag || curr !== currIter) {
-            clearInterval(ret);
-            return;
-        }
-        for (let j = 0; j < arrlen/width; j++) {
-            let k = j*width;
-            if (!done[j]) {
-                done[j] = true;
-                for (let i = 1; i < width; i++) {
-                    if (progress[k + i - 1] > progress[k + i]) {
-                        done[j] = false;
-                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
-                        progress[k + i] = progress[k + i] ^ progress[k + i - 1];
-                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
-                    }
-                }
-            }
-        }
-        for (let j = 0; j < arrlen; j += width) {
-            for (let i = 0; i < width; i++) {
-                var iter = (j + progress[j + i]) * 4;
-                var iter2 = (j + i) * 4;
-                target[iter2 + 0] = array[iter + 0];
-                target[iter2 + 1] = array[iter + 1];
-                target[iter2 + 2] = array[iter + 2];
-                target[iter2 + 3] = array[iter + 3];
-            }
-        }
-        draw();
-    }, timestep);
-    return this;
-}
-
-
 function cocktail(array, target, width, draw, curr) {
     var done = [];
     var progress = [];
     var arrlen = array.length / 4;
+    var sofar = [];
     for (let i = 0; i < arrlen; i++) {
-        progress[i] = i % (width+1);
+        progress[i] = i % width;
+    }
+    for (let i = 0; i < arrlen / width; i++) {
+        sofar[i] = 0;
     }
     for (let i = 0; i < arrlen; i += width) {
         done.push(false);
@@ -108,29 +54,32 @@ function cocktail(array, target, width, draw, curr) {
             clearInterval(ret);
             return;
         }
-        for (let j = 0; j < arrlen; j += width) {
+        for (let j = 0; j < arrlen/width; j++) {
+            let k = j*width;
             if (!done[j]) {
                 done[j] = true;
-                for (let i = 1; i < width; i++) {
-                    if (progress[j + i - 1] > progress[j + i]) {
+                for (let i = 1+sofar[j]; i < width-sofar[j]; i++) {
+                    if (progress[k + i - 1] > progress[k + i]) {
                         done[j] = false;
-                        progress[j + i - 1] = progress[j + i - 1] ^ progress[j + i];
-                        progress[j + i] = progress[j + i] ^ progress[j + i - 1];
-                        progress[j + i - 1] = progress[j + i - 1] ^ progress[j + i];
+                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
+                        progress[k + i] = progress[k + i] ^ progress[k + i - 1];
+                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
                     }
                 }
                 if (done[j]) {
                     break;
                 }
                 done[j] = true;
-                for (let i = width - 1; i > 0; i--) {
-                    if (progress[j + i - 1] > progress[j + i]) {
+                for (let i = width - 1-sofar[j]; i > sofar[j]; i--) {
+                    if (progress[k + i - 1] > progress[k + i]) {
                         done[j] = false;
-                        progress[j + i - 1] = progress[j + i - 1] ^ progress[j + i];
-                        progress[j + i] = progress[j + i] ^ progress[j + i - 1];
-                        progress[j + i - 1] = progress[j + i - 1] ^ progress[j + i];
+                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
+                        progress[k + i] = progress[k + i] ^ progress[k + i - 1];
+                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
                     }
                 }
+
+                sofar[j]++;
             }
         }
         for (let j = 0; j < arrlen; j += width) {
@@ -147,6 +96,71 @@ function cocktail(array, target, width, draw, curr) {
     }, timestep);
     return this;
 }
+
+
+
+
+function bubblesort(array, target, width, draw, curr) {
+    var done = [];
+    var progress = [];
+    var arrlen = array.length / 4;
+    var sofar = [];
+    for (let i = 0; i < arrlen; i++) {
+        progress[i] = i % width;
+    }
+    for(let i = 0; i < arrlen/width; i++) {
+        sofar[i] = 0;
+    }
+    for (let i = 0; i < arrlen; i += width) {
+        done.push(false);
+        var ret = progress.slice(i, i + width);
+        var shuf = shuffle(ret);
+        for(let j = 0; j < width; j++) {
+            progress[i+j]=shuf[j];
+        }
+    }
+
+
+    var ret = setInterval(function () {
+        var flag = true;
+        for (let j = 0; j < arrlen / width; j++) {
+            flag &= done[j];
+        }
+        if (flag || curr !== currIter) {
+            clearInterval(ret);
+            return;
+        }
+        for (let j = 0; j < arrlen/width; j++) {
+            let k = j*width;
+            if (!done[j]) {
+                done[j] = true;
+                for (let i = 1; i < width-sofar[j]; i++) {
+                    if (progress[k + i - 1] > progress[k + i]) {
+                        done[j] = false;
+                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
+                        progress[k + i] = progress[k + i] ^ progress[k + i - 1];
+                        progress[k + i - 1] = progress[k + i - 1] ^ progress[k + i];
+                    }
+                }
+
+                sofar[j]++;
+            }
+        }
+        for (let j = 0; j < arrlen; j += width) {
+            for (let i = 0; i < width; i++) {
+                var iter = (j + progress[j + i]) * 4;
+                var iter2 = (j + i) * 4;
+                target[iter2 + 0] = array[iter + 0];
+                target[iter2 + 1] = array[iter + 1];
+                target[iter2 + 2] = array[iter + 2];
+                target[iter2 + 3] = array[iter + 3];
+            }
+        }
+        draw();
+    }, timestep);
+    return this;
+}
+
 
 
 function processImg() {
@@ -179,7 +193,7 @@ function processImg() {
     var p2 = ctx2.createImageData(p);
     var pixels = p.data;
     var pixels2 = p2.data;
-    bubblesort(pixels, pixels2, p2.width, function () {
+    cocktail(pixels, pixels2, p2.width, function () {
         ctx2.putImageData(p2, 0, 0);
     }, currIter);
 }
