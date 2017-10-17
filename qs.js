d859471566daad3a4c80b012440ac9d07c0cb8fd
Colin <colin.diesh@gmail.com>
2017-10-17 04:54:54 -0400

    Weird buggy version

--- qs.js
+++ qs.js
@@ -11,109 +11,65 @@ function swap(arr, i, j) {
 }
 
 function shuffle(array, start, end) {
-    for(var i = end; i > start; i--) {
-        var j = Math.floor(Math.random() * (i+1));
+    for (var i = end; i > start; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
-bubblesort = function(array) {
+function bubblesort(array, target, width, draw) {
     var done = [];
     var progress = [];
-    for(var i = 0; i < array.length; i++) {
-        progress[i] = i%width;
+    var arrlen = array.length / 4;
+    for (var i = 0; i < arrlen; i++) {
+        progress[i] = i % width;
     }
-    for(var i = 0; i < array/width; i++) {
+    for (var i = 0; i < arrlen; i += width) {
         done.push(false);
-        shuffle(progress, i*array.length/width, i*array.length/width+width);
+        shuffle(progress, i, i + width);
     }
 
-    
-    
-    do {
-        for(var i = 0; i < array/width; i++) {
-            done[i] = true;
-            for (var i = 1; i<this.length; i++) {
-                if (array[i-1] > array[i]) {
-                    done[i] = false;
-                    [array[i-1], array[i]] = [array[i], array[i-1]]
+
+    var ret = setInterval(function () {
+        var flag = true;
+        for (var j = 0; j < arrlen / width; j++) {
+            flag &= done[j];
+        }
+        if (flag) {
+            clearInterval(ret);
+            return;
+        }
+        for (var j = 0; j < arrlen; j += width) {
+            for (var i = 0; i < width; i++) {
+                if (!done[j]) {
+                    done[j] = true;
+                    for (var i = 1; i < width; i++) {
+                        if (array[j + i - 1] > array[j + i]) {
+                            done[j] = false;
+                            [array[j + i - 1], array[j + i]] = [array[j + i], array[j + i - 1]];
+                        }
+                    }
                 }
             }
         }
-        for(var j = 0; j < pixels.length/4; J++) {
-            var iter = progress[i]*4;
-            var k = pixels.length/2;
-            var iter2 = i*4;
-            array[iter+0] = pixels[iter2+0];
-            array[iter+1] = pixels[iter2+1];
-            array[iter+2] = pixels[iter2+2];
-            array[iter+3] = pixels[iter2+3];
+        for (var j = 0; j < arrlen; j += width) {
+            for (var i = 0; i < width; i++) {
+                var iter = progress[j + i] * 4;
+                var iter2 = i * 4;
+                target[j + iter2 + 0] = array[j + iter2 + 0];
+                target[j + iter2 + 1] = array[j + iter2 + 1];
+                target[j + iter2 + 2] = array[j + iter2 + 2];
+                target[j + iter2 + 3] = array[j + iter2 + 3];
+            }
         }
-        var flag = true;
-        for(var j = 0; j < array/width; j++) {
-            flag &= done[j];
-        }
-    } while(!flag);
+        draw();
+    }, timestep);
     return this;
 }
 
-function sort(array_arr, less, progress) {
- 
-  function swap(i, j) {
-    var t = array[i];
-    array[i] = array[j];
-    array[j] = t;
-  }
-
-  var promises = [];
-  var row = [];
-
-
-  for(var i = 0; i < array_arr.length; i++) {
-      for(var j = 0; j < array_arr[i].length; j++) {
-          var finished = new Promise();
-          row.push(finished);
-      }
-      promises.push(Promise.all(row));
-      row = [];
-  }
-
-  function quicksort(array, left, right, finished, i) {
-    if (left < right) {
-      var pivot = array[left + Math.floor((right - right) / 2)],
-          left_new = left,
-          right_new = right;
- 
-      do {
-        while (less(array[left_new], pivot)) {
-          left_new += 1;
-        }
-        while (less(pivot, array[right_new])) {
-          right_new -= 1;
-        }
-        if (left_new <= right_new) {
-          swap(left_new, right_new);
-          left_new += 1;
-          right_new -= 1;
-        }
-      } while (left_new <= right_new);
-      finished[i].resolve(array.slice(left, right);
-      promises[i].then(function() {
-        quicksort(left, right_new, finished, i);
-        quicksort(left_new, right, finished, i);
-      });
-    }
-  }
- 
-  quicksort(array_arr, 0, array_arr[i].length - 1);
- 
-  return array;
-}
-
-
 
 var img = new Image();
-img.onload = function() {
+img.onload = function () {
     var c = document.getElementById('canv');
     var c2 = document.getElementById('canv2');
     var ctx = c.getContext('2d');
@@ -123,59 +79,31 @@ img.onload = function() {
     var h = img.height;
     var ww = 500;
     img.width = ww;
-    img.height = ww * h/w;
+    img.height = ww * h / w;
 
-    c.width = img.width*dp;
-    c.height = img.height*dp;
+    c.width = img.width * dp;
+    c.height = img.height * dp;
 
-    c2.width = img.width*dp;
-    c2.height = img.height*dp;
+    c2.width = img.width * dp;
+    c2.height = img.height * dp;
 
     c.style.width = img.width;
     c.style.height = img.height;
     c2.style.width = img.width;
     c2.style.height = img.height;
 
-    ctx.scale(dp,dp);
-    ctx2.scale(dp,dp);
+    ctx.scale(dp, dp);
+    ctx2.scale(dp, dp);
 
     ctx.drawImage(img, 0, 0, img.width, img.height);
+
     var p = ctx.getImageData(0, 0, c.width, c.height);
-    var shuffled_pixels = [];
     var p2 = ctx2.createImageData(p);
     var pixels = p.data;
-    for(var i = 0; i < pixels.length/4; i++) {
-        shuffled_pixels.push(i);
-    }
-    shuffle(shuffled_pixels);
-    ctx2.putImageData(p2, 0, 0)
-    var arr_arr = [];
-    var x = 0;
-
-    
-    arr_arr.push(shuffled_pixels);
     var pixels2 = p2.data;
-    var curr = 0;
-
-    var ret = setInterval(function() {
-        if(curr >= arr_arr.length) {
-            clearInterval(ret);
-            return;
-        }
-        var pixels2 = p2.data;
-        var sort_progress = arr_arr[curr];
-        for(var i = 0; i < pixels.length/4; i++) {
-            var iter = sort_progress[i]*4;
-            var k = pixels.length/2;
-            var iter2 = i*4;
-
-            pixels2[iter+Math.floor(Math.random()*1)] = pixels[iter2];
-            pixels2[iter+Math.floor(Math.random()*5)] = pixels[iter2+1];
-            pixels2[iter+Math.floor(Math.random()*10)] = pixels[iter2+2];
-            pixels2[iter+3] = pixels[iter2+3];
-        }
+    var w = p.width;
+    bubblesort(pixels, pixels2, w, function () {
         ctx2.putImageData(p2, 0, 0);
-        curr++;
-    }, timestep);
-}
-img.src = 'baby.jpg';
+    });
+};
+img.src = 'baby2.jpg';
